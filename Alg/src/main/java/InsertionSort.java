e664fd1670d5f03fc012554279c62c56f685fc5a
Chun Zheng <zhengc42@gmail.com>
2018-06-12 23:49:58 -0400

    Alg : this algorithms book sucks a bit

--- Alg/src/main/java/InsertionSort.java
+++ Alg/src/main/java/InsertionSort.java
@@ -1,63 +1,55 @@
 import java.util.*;
 
 public class InsertionSort {
+    public InsertionSort() {
+    }
 
-  public static void main(String[] args) {
-    // input  sequence of n numbers (a1, a2, ... a(n))
-    // the numbers you want to sort are keys
-    // output: sorted array i guess
-    ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(9, 32, 12314,1,6457,236,23,2,3,42,167,4,23,1));
-    System.out.println(personatlInsertionSort(nums));
-    System.out.println(CLRSInsertionSort(nums));
-  }
+    public static void main(String[] args) {
+        // input  sequence of n numbers (a1, a2, ... a(n))
+        // the numbers you want to sort are keys
+        // output: sorted array i guess
+        ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(9, 32, 12314,1,6457,236,23,2,3,42,167,4,23,1));
+        InsertionSort insertionSort = new InsertionSort();
+        System.out.println(insertionSort.personalInsertionSort(nums));
+        System.out.println(insertionSort.CLRSInsertionSort(nums));
+    }
 
 
     // I totally did this wrong on first breadth of the chapter
-    private String personalInsertionSort(ArrayList<Integer> nums) {
-      // sorts in place "apparently"
-      int len = nums.size();
-      for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-          if (nums.get(i) > nums.get(j)) {
-            Integer temp = nums.get(i);
-            nums.set(i, nums.get(j));
-            nums.set(j, temp);
-          }
+    public String personalInsertionSort(ArrayList<Integer> nums) {
+        // sorts in place "apparently"
+        long startTime = System.nanoTime();
+        int len = nums.size();
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums.get(i) > nums.get(j)) {
+                    Integer temp = nums.get(i);
+                    nums.set(i, nums.get(j));
+                    nums.set(j, temp);
+                }
+            }
         }
-      }
-
-      return nums.toString();
+        long endTime = System.nanoTime();
+        System.out.println("Total execution time: " + (endTime - startTime) );
+        return nums.toString();
     }
 
     // proper insertion
-    private String CLRSInsertionSort(ArrayList<Integer> nums) {
-      for( int j = 1; j < nums.size(); j++) {
-        key = nums.get(j);
-        int i = j -1;
-        while (i > 0 && nums.get(i) > key) {
-          nums.get(i + 1) = nums.get(i);
-          i = i -1;
+    public String CLRSInsertionSort(ArrayList<Integer> nums) {
+        long startTime = System.nanoTime();
+        if (nums.size() > 1) {
+            for( int j = 1; j < nums.size(); j++) {
+                Integer key = nums.get(j);
+                int i = j -1;
+                while (i > 0 && nums.get(i) > key) {
+                    nums.set(i + 1, nums.get(i));
+                    i = i -1;
+                }
+                nums.set(i + 1, key);
+            }
         }
-        nums.get(i + 1) = key;
-      }
-
-      return nums.toString();
+        long endTime = System.nanoTime();
+        System.out.println("Total execution time: " + (endTime - startTime) );
+        return nums.toString();
     }
-
-    private String personalIntepretationSort(ArrayList<Integer> cards) {
-      int numberOfCards = cards.size();
-      for(cardsOnTable = 2; cardsOnTable < numberOfCards; cardsOnTable++) {
-        cardInLeftHand = cards[cardsOnTable];
-
-        cardInRightHand = cardsOnTable - 1;
-
-        while (cardInRightHand > 0 && card.get(cardInRightHand) > cardInLeftHand) {
-          cards.get(cardInLeftHand + 1) = card.get(cardInLeftHand);
-          cardInLeftHand = cardInLeftHand - 1;
-        }
-        cards.get(cardInRightHand) = cardInLeftHand;
-
-      }
-    }
-  }
 }
