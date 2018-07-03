3b0e1a278a9a54ea5afb1b47aee4d1d46f2a9d9d
chun <chun@DESKTOP-OF4VCI6.localdomain>
2018-07-02 22:30:43 -0400

    CheckPermutations

---
+++ CrackingTheCodingInterview/Chapter0/CheckPermutation.java
@@ -0,0 +1,26 @@
+/**
+ * given two strings, write a method to decide if one is a permutation of the other.
+ */
+import java.util.Arrays;
+public class CheckPermutation {
+    public static void main(String[] args) {
+        CheckPermutation ck = new CheckPermutation();
+        System.out.println(ck.permutates("kitten", "nettik"));
+        System.out.println(ck.permutates("dog", "god"));
+        System.out.println(ck.permutates("man", "cad"));
+        System.out.println(ck.permutates("woman", "nam"));
+    }
+
+    public boolean permutates(String word, String secondWord) {
+        if(word.length() != secondWord.length()) {
+            return false;
+        }
+        return sortString(word).equals(sortString(secondWord));
+    }
+
+    public String sortString(String s) {
+        char[] tempArray = s.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+}
