1b368ef3c7ed2bcddbf620758ac97d1da91c25b4
zhengc42 <zhengc42@gmail.com>
2018-10-04 01:01:47 -0400

    PalindromeNumber

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,22 +1,27 @@
 import java.util.*;
-public class Selection extends SortParent {
+public class SelectionSort extends SortParent {
     public static void main(String[] args) {
-        SelectionSort bs = new SelectionSort();
-        bs.sort();
+        SelectionSort ss = new SelectionSort();
+        ss.sort();
     }
 
     /**
-     * buble sort is n^2 at worst case
-     * then it's n in it's best case
      */
     public int[] sort() {
         int[] answers = numbers;
         int n = numbers.length;
+        printArray();
         for (int i = 0; i < n; i++) {
-            int temp = 0;
+            int min_index = i;
             for (int j = i; j < n; j++) {
-                if (
+                if (numbers[j] < numbers[min_index]) {
+                    min_index = j;
+                }
             }
+            int temp = numbers[min_index];
+            numbers[min_index] = numbers[i];
+            numbers[i] = temp;
+            printArray();
         }
         return answers;
     }
