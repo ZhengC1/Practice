8eaf7f7557e2bf805471eb732d5d4bf7bdf449f6
Chun <zhengc42@gmail.com>
2018-10-06 19:20:30 -0400

    insertionSort

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class InsertionSort extends SortParent {
+    public static void main(String[] args) {
+        InsertionSort is = new InsertionSort();
+        is.sort();
+    }
+    public int[] sort() {
+        // so you start at the second index and loop forward
+        for(int i = 1; i < n;i++) {
+            // the index before i, so first iteration will be the first index
+            int current_element = numbers[i];
+            int j = i -1;
+            // loops backwards and checks if the current index element is smaller than the ones before it.
+            while(j >= 0 && current_index < numbers[j]) {
+                // swapping
+                int temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+                j--;
+            }
+        }
+        return list;
+    }
+}
