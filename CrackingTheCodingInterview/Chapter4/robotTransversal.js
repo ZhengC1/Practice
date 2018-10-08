3855eb98908f29570aef9c9f36d8d15b334dd0e0
Chun Zheng <chun.zheng@bronto.com>
2018-10-07 21:15:33 -0400

    adding chapter4 questions

---
+++ CrackingTheCodingInterview/Chapter4/robotTransversal.js
@@ -0,0 +1,35 @@
+const mapGenerator = size => {
+    var arr = []
+    for(let i = 0; i < size; i++) {
+        var row = [];
+        for(let j = 0; j < size; j++) {
+            row.push(Math.round(Math.random()));
+        }
+        arr.push(row);
+    }
+    arr[0][0] = -1;
+    arr[arr.length -1][arr.length -1] = -1;
+    return arr;
+}
+
+
+const visitedBool = size => {
+    var arr =[];
+    for(let i = 0; i < size; i++) {
+        var row = [];
+        for(let j = 0; j < size; j++) {
+            row.push(false);
+        }
+        arr.push(row);
+    }
+    return arr;
+}
+
+
+const size = 5;
+let map = mapGenerator(size);
+let visited = visitedBool(size);
+
+const bfs = (map, visited) => {
+
+}
