23403a19a13b28be93843c60a0725a6c2982b684
zhengc42 <zhengc42@gmail.com>
2018-10-14 00:55:49 -0400

    added quick sort skeleton

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,14 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        QuickSort qs = new QuickSort();
+        qs.sort()
+    }
+
+    /**
+     * buble sort is n^2 at worst case
+     * then it's n in it's best case
+     */
+    public int[] sort() {
+    }
+
+}
