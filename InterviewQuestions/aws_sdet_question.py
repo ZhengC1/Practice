5064a10f6f7f97cd6e89c7cfdef03c12df6dd2c8
Chun Zheng <zhengc42@gmail.com>
2020-11-05 01:11:36 -0500

    added maze stuff for dfs

---
+++ InterviewQuestions/aws_sdet_question.py
@@ -0,0 +1,16 @@
+"""
+|X|S|X|X|X|
+|X| |X|X|I|
+|X| | | | |
+|X|X|X|X|X|
+"""
+
+visited = []
+size
+def reverse_bfs(plot:List, location) -> List[str]:
+    if location in visited:
+        return
+    elif plot[location[0], location[1]] == 'X' or 
+    visited.append(location)
+
+
---
+++ maze/breadth_first/tests/test_breadth_first.py
@@ -0,0 +1,5 @@
+from breadth_first import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
